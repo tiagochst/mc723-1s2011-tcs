d36fec62ab4714aea1e95d7fea8590a6ebb7478d
tiagochst <tiagochst@2ca02b38-4dbf-e3b2-950b-5a29be7df633>
2011-03-28 16:50:11 +0000

    Lab quase no fim

--- trunk/lab2/plotdata/calc.py
+++ trunk/lab2/plotdata/calc.py
@@ -5,33 +5,41 @@ import fileinput
 
 maq=[(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),(8,0),(9,0),(10,0),(11,0)]
 class Desempenho:
-	def __init__(self, pc, time):
+	def __init__(self,prog, pc, time):
+		self.prog = prog
 		self.pc = pc
 		self.time = time
 	
 	def __repr__(self):
-		return repr((self.pc, self.time))
+		return repr((self.prog,self.pc, self.time))
 
-def main(i):
+def main():
 	obj=[]
 	obj2=[]
-	for line in fileinput.input(['prog'+str(i)+'.dat']):
-		if not fileinput.isfirstline():
+	for i in range(1,11):
+		for line in fileinput.input(['prog'+str(i)+'.dat']):
+			if not fileinput.isfirstline():
 			#print line
-			line = line.split()	
-			obj.append(Desempenho(eval(line[1]),eval(line[2])))
+				line = line.split()	
+				obj.append(Desempenho(eval(line[0]),eval(line[1]),eval(line[2])),) #numero prog,numero pc, tempo
+			
 	obj2=sorted(obj,key=lambda desempenho: desempenho.time)
+	obj=sorted(obj2,key=lambda desempenho: desempenho.prog)
 	print "=== Maquina"+str(i)+"====" 
-	print obj2
+	print obj
 	print "========================="
-	for i in range(len(obj2)):
-		maq[obj2[i].pc]=((maq[obj2[i].pc])[0]+i,maq[(obj2[i]).pc][1]+1)
-		
+	pos=0
+	for i in range(len(obj)):
+		aux=obj[i].prog
+		maq[obj[i].pc]=((maq[obj[i].pc])[0]+pos,maq[(obj[i]).pc][1]+1)
+		pos=pos+1
+		if i!= (len(obj)-1):
+			if aux!=obj[i+1].prog:
+				pos=0
 
 if __name__ == "__main__":
-	for i in range(1,11):
-		main(i)
+	main()
 
 	for i in range(1,12):
-		print (((maq[i])[0])/((maq[i])[1]))
+		print float(float((maq[i])[0])/float((maq[i])[1]))
 	print maq
